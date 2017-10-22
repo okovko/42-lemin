379e90ce22b82120231b6150411f5f1be02d8f8c
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-10-22 01:38:46 -0700

    added check_ants

---
+++ srcs/check_ants.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_ants.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olkovale <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/22 01:38:16 by olkovale          #+#    #+#             */
+/*   Updated: 2017/10/22 01:38:16 by olkovale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "lemin.h"
+
+t_bool	check_ants(char *ss)
+{
+	if (!ISDIGIT(*ss))
+		return (false);
+	while (ISDIGIT(*ss))
+		ss++;
+	if ('\0' != *ss)
+		return (false);
+	return (true);
+}
