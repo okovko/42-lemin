31c49e5792b50015d18828219bd7396f590bbf65
Oleg Kovalenko <olkovale@e1z1r4p20.42.us.org>
2017-10-27 17:27:23 -0700

    norm

--- srcs/parse_lines.c
+++ srcs/parse_lines.c
@@ -25,7 +25,7 @@ t_lst	*parse_lines(void)
 		ft_putstr("\n");
 		if ('#' != *ss || 0 == strcmp_strict("##start", ss)
 			|| 0 == strcmp_strict("##end", ss))
-		lines = ft_lstadd(&lines, ft_lstnode(ss, ft_strlen(ss)));
+			lines = ft_lstadd(&lines, ft_lstnode(ss, ft_strlen(ss)));
 	}
 	return (ft_lstsrev(&lines));
 }
